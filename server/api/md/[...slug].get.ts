576508ce7d516b59e186f1098109b675a2034820
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-12 23:46:03 -0400

    feat: initialize endpoint that will be used to return the markdown version of a page

---
+++ server/api/md/[...slug].get.ts
@@ -0,0 +1,32 @@
+import { stringify } from "minimark/stringify";
+import { withLeadingSlash } from "ufo";
+import { object, string } from "yup";
+
+const slugSchema = object({
+  slug: string().min(2).max(100).required().label("Slug"),
+});
+
+export default defineEventHandler(async (event) => {
+  // validate the slug parameter from the request
+  const { slug } = await getValidatedRouterParams(event, (d) => slugSchema.validateSync(d));
+  // get the page from the docs collection
+  const page = await queryCollection(event, "content").path(withLeadingSlash(slug)).first();
+  if (!page) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Page not found",
+      message: `No page found for slug: ${slug}`,
+      data: { slug },
+    });
+  }
+
+  // Add title and description to the top of the page if missing
+  if (page.body.value[0]?.[0] !== "h1") {
+    page.body.value.unshift(["blockquote", {}, page.description]);
+    page.body.value.unshift(["h1", {}, page.title]);
+  }
+
+  setHeader(event, "Content-Type", "text/markdown; charset=utf-8");
+
+  return stringify({ ...page.body, type: "minimark" }, { format: "markdown/html" });
+});
