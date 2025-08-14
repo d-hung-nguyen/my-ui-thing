efcdd40281d1a81b4e75b6b70ddead039ce0f912
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-13 22:15:55 -0400

    feat: add shortcut definitions for question mark and G D key combination

---
+++ app/components/content/Docs/Shortcuts/ShortcutsExThree.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+  defineShortcuts({
+    "?": () => useSonner.info("Question Mark", { description: "This is the question mark key." }),
+    "g-d": () => useSonner.info("G D", { description: "This is the G D key combination." }),
+  });
+</script>
