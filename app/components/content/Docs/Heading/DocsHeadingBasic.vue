53a40ee171e7cada4cc71d6226e9071f76ee790c
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-15 11:43:08 -0400

    chore: add docs for `heading` component

---
+++ app/components/content/Docs/Heading/DocsHeadingBasic.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid gap-2">
+    <ui-heading>{{ text }}</ui-heading>
+    <ui-heading :level="2">{{ text }}</ui-heading>
+    <ui-heading :level="3">{{ text }}</ui-heading>
+    <ui-heading :level="4">{{ text }}</ui-heading>
+    <ui-heading :level="5">{{ text }}</ui-heading>
+    <ui-heading :level="6">{{ text }}</ui-heading>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const text = "The quick brown fox jumps over the lazy dog.";
+</script>
