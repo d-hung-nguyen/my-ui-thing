c3dd1b2a0b7f5ec707b9047613c522fc08e94643
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-08-22 16:48:37 -0400

    feat: enhance SourceCodeLink component with tooltip and dynamic URL generation

--- app/components/content/SourceCodeLink.vue
+++ app/components/content/SourceCodeLink.vue
@@ -1,22 +1,43 @@
 <template>
-  <a :href="sourceCodeUrl" target="_blank" :title="titleText">
-    {{ text || "here" }}
-  </a>
+  <ui-tooltip>
+    <ui-tooltip-trigger>
+      <a :href="sourceCodeUrl" target="_blank">
+        {{ text || "here" }}
+        <span class="sr-only"> {{ titleText }}</span>
+      </a>
+    </ui-tooltip-trigger>
+    <ui-tooltip-content
+      >{{ titleText }}
+      <ui-tooltip-arrow />
+    </ui-tooltip-content>
+  </ui-tooltip>
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps<{
-    text?: string;
-    component: string;
-  }>();
+  import type { Collections } from "@nuxt/content";
+
+  const props = withDefaults(
+    defineProps<{
+      text?: string;
+      branch?: string;
+      folder?: string;
+      component: string;
+      collection?: keyof Collections;
+    }>(),
+    {
+      branch: "v2",
+      folder: "Ui",
+      collection: "content",
+    }
+  );
 
   const sourceCodeUrl = computed(() => {
-    return `https://github.com/BayBreezy/ui-thing/tree/main/app/components/Ui/${props.component}`;
+    return `https://github.com/BayBreezy/ui-thing/tree/${props.branch}/app/components/${props.folder}/${props.component}`;
   });
 
   const route = useRoute();
   const { data: page } = await useAsyncData(route.path, () => {
-    return queryCollection("content").path(route.path).first();
+    return queryCollection(props.collection).path(route.path).first();
   });
   const titleText = computed(() =>
     page?.value ? `View source code for ${page.value.title}` : "View source code"
