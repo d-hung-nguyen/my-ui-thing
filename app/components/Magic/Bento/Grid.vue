1ed38dba412ee162d4ebe6d1477008057f886301
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-09-06 19:10:25 -0400

    feat: add BentoCard and BentoGrid components with responsive layout and features showcase

---
+++ app/components/Magic/Bento/Grid.vue
@@ -0,0 +1,15 @@
+<template>
+  <Primitive :class="styles({ class: props.class })">
+    <slot />
+  </Primitive>
+</template>
+
+<script lang="ts" setup>
+  import type { PrimitiveProps } from "reka-ui";
+  import type { HTMLAttributes } from "vue";
+
+  const props = defineProps<PrimitiveProps & { class?: HTMLAttributes["class"] }>();
+  const styles = tv({
+    base: "grid w-full auto-rows-[22rem] grid-cols-3 gap-4",
+  });
+</script>
---
+++ app/components/content/Magic/BentoGrid/MagicDocsBentoGrid.vue
@@ -0,0 +1,56 @@
+<template>
+  <MagicBentoGrid class="lg:grid-rows-3">
+    <MagicBentoCard v-for="item in features" :key="item.name" v-bind="item" />
+  </MagicBentoGrid>
+</template>
+
+<script lang="ts" setup>
+  const bgImage = h("img", { class: "absolute -right-20 -top-20 opacity-60" });
+  const features = [
+    {
+      icon: "lucide:file-text",
+      name: "Save your files",
+      description: "We automatically save your files as you type.",
+      href: "/",
+      cta: "Learn more",
+      background: bgImage,
+      class: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
+    },
+    {
+      icon: "lucide:text-cursor-input",
+      name: "Full text search",
+      description: "Search through all your files in one place.",
+      href: "/",
+      cta: "Learn more",
+      background: bgImage,
+      class: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
+    },
+    {
+      icon: "lucide:globe",
+      name: "Multilingual",
+      description: "Supports 100+ languages and counting.",
+      href: "/",
+      cta: "Learn more",
+      background: bgImage,
+      class: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
+    },
+    {
+      icon: "lucide:calendar",
+      name: "Calendar",
+      description: "Use the calendar to filter your files by date.",
+      href: "/",
+      cta: "Learn more",
+      background: bgImage,
+      class: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
+    },
+    {
+      icon: "lucide:bell",
+      name: "Notifications",
+      description: "Get notified when someone shares a file or mentions you in a comment.",
+      href: "/",
+      cta: "Learn more",
+      background: bgImage,
+      class: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
+    },
+  ];
+</script>
