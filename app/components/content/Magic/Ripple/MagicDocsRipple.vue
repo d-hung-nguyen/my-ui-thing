3d5787e4acced597977f3a1d178a3c8bc304a2f0
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-09-17 18:32:18 -0400

    feat: add Ripple component with customizable properties and update documentation

---
+++ app/components/content/Magic/Ripple/MagicDocsRipple.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background"
+  >
+    <p
+      class="z-10 text-center text-5xl font-medium tracking-tighter whitespace-pre-wrap text-white"
+    >
+      Ripple
+    </p>
+    <MagicRipple />
+  </div>
+</template>
