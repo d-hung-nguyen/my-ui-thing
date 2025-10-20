078b87911d4754020dc39798660f3128c116a41a
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-10-20 19:34:59 -0400

    feat: add ProseP component for paragraph rendering with customizable classes

---
+++ app/components/content/prose/Typography/ProseP.vue
@@ -0,0 +1,24 @@
+<template>
+  <p data-slot="prose-p" :class="prosePStyles({ class: _class })">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts">
+  import type { HTMLAttributes } from "vue";
+
+  export type ProsePProps = {
+    /**
+     * Additional classes for the parent element
+     */
+    class?: HTMLAttributes["class"];
+  };
+
+  export const prosePStyles = tv({
+    base: "leading-7 text-pretty [&:not(:first-child)]:mt-6",
+  });
+</script>
+
+<script setup lang="ts">
+  const { class: _class } = defineProps<ProsePProps>();
+</script>
