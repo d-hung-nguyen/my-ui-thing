149b74b3450a8f9d1dbe3574df67819d95ebd134
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2025-10-27 09:14:20 -0400

    fix: update class syntax in RetroGrid component for consistency and clarity

--- app/components/Ui/RetroGrid.vue
+++ app/components/Ui/RetroGrid.vue
@@ -11,15 +11,13 @@
       '--dark-line': darkLineColor,
     }"
   >
-    <div class="absolute inset-0 [transform:rotateX(var(--grid-angle))]">
+    <div class="absolute inset-0 transform-[rotateX(var(--grid-angle))]">
       <div
-        class="[inset:0%_0px] [margin-left:-200%] [height:300vh] [width:600vw] [transform-origin:100%_0_0] animate-grid [background-image:linear-gradient(to_right,var(--light-line)_1px,transparent_0),linear-gradient(to_bottom,var(--light-line)_1px,transparent_0)] [background-size:var(--cell-size)_var(--cell-size)] [background-repeat:repeat] dark:[background-image:linear-gradient(to_right,var(--dark-line)_1px,transparent_0),linear-gradient(to_bottom,var(--dark-line)_1px,transparent_0)]"
+        class="inset-[0%_0px] -ml-[200%] h-[300vh] w-[600vw] origin-[100%_0_0] animate-grid bg-[linear-gradient(to_right,var(--light-line)_1px,transparent_0),linear-gradient(to_bottom,var(--light-line)_1px,transparent_0)] bg-size-[var(--cell-size)_var(--cell-size)] bg-repeat dark:bg-[linear-gradient(to_right,var(--dark-line)_1px,transparent_0),linear-gradient(to_bottom,var(--dark-line)_1px,transparent_0)]"
       />
     </div>
 
-    <div
-      class="absolute inset-0 bg-gradient-to-t from-white to-transparent to-90% dark:from-black"
-    />
+    <div class="absolute inset-0 bg-linear-to-t from-white to-transparent to-90% dark:from-black" />
   </Primitive>
 </template>
 
@@ -71,8 +69,8 @@
   });
   const styles = tv({
     base: [
-      "pointer-events-none absolute size-full overflow-hidden [perspective:200px]",
-      "opacity-[var(--opacity)]",
+      "pointer-events-none absolute size-full overflow-hidden perspective-[200px]",
+      "opacity-(--opacity)",
     ],
   });
 </script>
